2ecfb97577bb6802ecebe917bb2828a2285334e2
Lukass860 <lukas.shatilovs@gmail.com>
2026-08-05 16:35:33 +0000

    start

---
+++ learning.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello Lukass!";
+    return 0;
+}
+/*#include <iostream> — loads the library that lets you print text to the screen.
+int main() { ... } — this is the main function, where every C++ program starts running.
+cout << "Hello, World!"; — prints the text Hello, World! to the screen. g++ learning.cpp -o learning ./learning*/
