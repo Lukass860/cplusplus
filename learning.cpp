669c89338b7eb6a0e79bb6b1e56197a8c216fa2f
Lukass860 <lukas.shatilovs@gmail.com>
2026-08-06 17:12:04 +0000

    tutorial

--- learning.cpp
+++ learning.cpp
@@ -6,3 +6,10 @@ int main() {
 /*#include <iostream> — loads the library that lets you print text to the screen.
 int main() { ... } — this is the main function, where every C++ program starts running.
 cout << "Hello, World!"; — prints the text Hello, World! to the screen. g++ learning.cpp -o learning ./learning*/
+
+/*In C++, we use )std::cout) to print output to the console.
+The text to be printed is placed within double quotes and followed by the insertion operator <<.
+Every statement in C++ must end with a semicolon ;.*/
+
+/*The iostream library provides the tools needed for input and output — without it,
+std::cout would not be available.*/
